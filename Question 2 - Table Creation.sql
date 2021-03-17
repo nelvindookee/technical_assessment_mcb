190c33327d34426f9852bed85d87ba39bc975bef
Dookee Nelvin <nelvindookee@gmail.com>
2021-03-17 20:53:13 +0400

    Add files via upload
    MCB Technical Assessment

---
+++ Question 2 - Table Creation.sql
@@ -0,0 +1,6 @@
+select * from XXBCM_SUPPLIER;
+select * from XXBCM_INVOICE_STATUS;
+select * from XXBCM_INVOICE_HOLD_REASON;
+select * from XXBCM_ORDER_STATUS;
+select * from XXBCM_ORDER;
+select * from XXBCM_INVOICE;
